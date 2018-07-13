6da5045929c392516e747e71ebe1b8d7f36b7ae8
camistan2105 <camille.jalliffier.verne@outlook.com>
2018-07-13 18:22:57 -0400

    suite games part

--- application/models/GamesModel.php
+++ application/models/GamesModel.php
@@ -3,12 +3,16 @@
 class GamesModel extends CI_Model
 {
 
-	protected $table = "games";
+	private $table_teams = "teams";
+	private $table_teams_players = "teams_players";
+	private $table_players = "players";
+	private $table_games = "games";
 	
 	public function get_games()
 	{
-		return $this->db->select('*')
-				->from($this->table)
+		return $this->db->select('g.id, g.date, g.team1_goals, g.team2_goals, g.is_played, t1.name as team1_name, t2.name as team2_name')
+				->from($this->table_games . ' as g, ' . $this->table_teams . ' as t1, ' . $this->table_teams . ' as t2')
+				->where('g.team1_id = t1.id AND g.team2_id = t2.id')
 				->order_by('date', 'desc')
 				->get()
 				->result();
--- application/views/games/GamesListView.php
+++ application/views/games/GamesListView.php
@@ -2,7 +2,25 @@
 
 	<div class="list-group">
 		<?php foreach ($games as $game): ?>
-			<button type="button" class="list-group-item list-group-item-action team-list" onclick="showGameCard(this, <?= $game->id ?>, '<?= site_url('Games/get_game_data') ?>')" ><?= $game->date ?></button>
+			<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+    			<div class="d-flex justify-content-end">
+      				<small><?= date( 'd/m/Y', strtotime( $game->date ) ) ?></small>
+    			</div>
+				<div class="d-flex justify-content-center">
+					<div class="d-flex flex-column">
+						<div class="p-2 d-flex justify-content-center"><?= $game->team1_name ?></div>
+						<div class="p-2 d-flex justify-content-center"><?= $game->team1_goals ?></div>
+					</div>
+					<div class="d-flex flex-column">
+						<div class="p-2 d-flex justify-content-center">VS</div>
+						<div class="p-2 d-flex justify-content-center"></div>
+					</div>
+					<div class="d-flex flex-column">
+						<div class="p-2 d-flex justify-content-center"><?= $game->team2_name ?></div>
+						<div class="p-2 d-flex justify-content-center"><?= $game->team2_goals ?></div>
+					</div>
+				</div>
+  			</a>
 		<?php endforeach; ?>
 	</div>
 
